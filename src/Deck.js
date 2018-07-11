d8dd4490f8f81d79730fd60867725503b0b58d47
amangotchu <aman.gottu@gmail.com>
2018-07-11 04:58:07 -0500

    Added Panhandler to whole deck. Used state for position and used Animated.View to give the deck coordinates with position.getLayout()

--- src/Deck.js
+++ src/Deck.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
-import { View, Text, Animated } from 'react-native';
+import { View, Text, Animated, PanResponder } from 'react-native';
 
 class Deck extends Component{
+
+  constructor(props) {
+    super(props);
+
+
+    const position = new Animated.ValueXY();
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+          position.setValue({ x: gesture.dx, y: gesture.dy})
+      },
+      onPanResponderRelease: () => {}
+    });
+
+    this.state = { panResponder, position };
+  }
 
   renderCards(){
     return this.props.data.map(item => {
@@ -11,9 +27,14 @@ class Deck extends Component{
 
   render(){
     return(
-      <View>
+
+      <Animated.View
+        style={this.state.position.getLayout()}
+        {...this.state.panResponder.panHandlers}
+      >
         {this.renderCards()}
-      </View>
+      </Animated.View>
+
     );
   }
 }
